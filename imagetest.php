e76711db64711e23d88640ef3b8e93ab9df82086
Vignesh Shankar <42212364+Vignesh0404@users.noreply.github.com>
2019-06-10 16:50:36 +0530

    update (#5)

---
+++ imagetest.php
@@ -0,0 +1,26 @@
+<?php
+  include 'connection.php';
+
+  session_start();
+
+  if(isset($_SESSION['user']))
+    $user = $_SESSION['user'];
+  else
+    header("Location:index.php");
+
+  $user = $_SESSION['user'];
+  $pid = $_GET['pid'];
+
+  $sql = "SELECT * FROM project WHERE pid='".$pid."';";
+  $result = $conn->query($sql);
+
+  $url = null;
+  if($row = $result->fetch_assoc())
+  {
+    $url = $row['image'];
+    $title = $row['title'];
+    $doneby = $row['uname'];
+    $tags = $row['tags'];
+  }
+
+  ?>
