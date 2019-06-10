7c70b7a07b15e4b5b720ef409868fae62bd0437b
gsnvsuraj <34811429+gsnvsuraj@users.noreply.github.com>
2019-06-10 17:15:21 +0530

    Merge Inter/changes (#4) -> master
    * separated header files
    * Update myProjects.php
    * updated projects and admin verification
    * Optimised the header files
    * separated verification mail sender
    * adding description imgdisplay
    * Added more fields in upload form
    * minor
    * added php mailer

--- filteredAdmin.php
+++ filteredAdmin.php
@@ -2,56 +2,18 @@
 <html>
 <head>
 	<title>Projects</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
- 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-	<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/balzss/luxbar/ae5835e2/build/luxbar.min.css">
-  <link rel="stylesheet" type="text/css" href="styles/projects.css">
-  <link rel="stylesheet" type="text/css" href="styles/styles.css">
-	<link rel="stylesheet" type="text/css" href="lib/textbox-css/textbox.css">
+	
+	<link rel="stylesheet" type="text/css" href="styles/projects.css">
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+	
 </head>
-<!-- Navigation Bar Open -->
-<header id="luxbar" class="luxbar-fixed">
-  <input type="checkbox" class="luxbar-checkbox" id="luxbar-checkbox" />
-  <div class="luxbar-menu luxbar-menu-right luxbar-menu-dark">
-    <ul class="luxbar-navigation">
-      <li class="luxbar-header">
-        <a href="#" class="luxbar-brand"><img src="assets/img/logo.png" style="width: 40%; height: 40%" alt="Bhumi Logo"></a> <label class="luxbar-hamburger luxbar-hamburger-doublespin" id="luxbar-hamburger" for="luxbar-checkbox">
-	       <span></span>
-	        </label>
-	      </li>
-	      <li class="luxbar-item"><a class="active" href="projectsAdmin.php">Home</a></li>
-	      <li class="luxbar-item"><a href="approval.php">Requets</a></li>
-	      <li class="luxbar-item">
-				<a>
-					<!-- Search Bar -->
-					<form action='filteredAdmin.php' method='post'>
-						<input type='text' name='filter' class="textbox7" placeholder='Filter by tags' required/>
-	      		<input type='submit' value='Filter' />
-	 				</form>
-				  </a>
-				</li>
-				<li class="luxbar-item"><a href="notificationAdmin.php">Notifications</a></li>
-	      <li class="luxbar-item"><a href="logout.php">Sign Out</a></li>
-	    </ul>
-	  </div>
-	</header>
-	<!-- Navigation Bar Close -->
+
 	<!-- Body Opens -->
-	<body>
+<body>
 		<br><br><br><!-- Too Lazy for Padding -->
 	<?php
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
-		$dbname = "uplabs";
-
-		// Create connection
-		$conn = new mysqli($servername, $username, $password, $dbname);
-
-		// Check connection
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		}
+		include 'headerAdmin.php';
+        include 'connection.php';
 
         $search = strtolower($_POST['filter']);
         $search = str_replace(" ",",",$search);
--- resetPass.php
+++ resetPass.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>Reset Password</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link rel="stylesheet" type="text/css" href="styles/styles.css">
 	<link rel="icon" type="image/ico" href="images/logo.png" />
