5443f3ea8e77d0d3de59e8d90dfe7c7c6be72021
gsnvsuraj <34811429+gsnvsuraj@users.noreply.github.com>
2019-06-10 12:04:39 +0530

    Minor Modifications

--- verify.php
+++ verify.php
@@ -6,18 +6,7 @@
 <body>
 
 	<?php
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
-		$dbname = "uplabs";
-
-		// Create connection
-		$conn = new mysqli($servername, $username, $password, $dbname);
-
-		// Check connection
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		}
+		include 'connection.php';
 
 		$uid = $_GET['uid'];
 
@@ -27,6 +16,8 @@
 		echo "<center><h3>Your Account has been verified. Now you can login.</h3></center>";
 
 		sleep(20);
+
+		$conn->close();
 
 		header('location:index.php');
 		exit();
