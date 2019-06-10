cbbe59d977b9dc7e2285124dc81bba89b83ee86b
Krishna Alagiri (KK) <krishna.alagiri03@gmail.com>
2019-06-10 18:28:16 +0530

    Update

--- header.php
+++ header.php
@@ -8,13 +8,20 @@
     <input type="checkbox" class="luxbar-checkbox" id="luxbar-checkbox"/>
     <div class="luxbar-menu luxbar-menu-right luxbar-menu-dark">
         <ul class="luxbar-navigation">
-            <li class="luxbar-header">
-                <a href="#" class="luxbar-brand">CREATIVES</a>
-                <label class="luxbar-hamburger luxbar-hamburger-doublespin"
-                id="luxbar-hamburger" for="luxbar-checkbox"> <span></span> </label>
-            </li>
+          <li class="luxbar-header">
+              <a href="#" class="luxbar-brand"><img src="assets/img/logo.png" style="width: 40%; height: 40%" alt="Bhumi Logo"></a> <label class="luxbar-hamburger luxbar-hamburger-doublespin" id="luxbar-hamburger" for="luxbar-checkbox">
             <li class="luxbar-item"><a href="projects.php">Home</a></li>
             <li class="luxbar-item"><a href="myProjects.php">Projects</a></li>
+            <li class="luxbar-item">
+                <a>
+                    <!-- Search Bar -->
+                    <form action='filtered.php' method='post'>
+                        <input type='text' name='filter' placeholder='Filter by tags' required/>
+                        <input type='submit' value='Filter' />
+                    </form>
+                </a>
+            </li>
+
             <li class="luxbar-item"><a href="uploading.php">Uploads</a></li>
             <li class="luxbar-item"><a href="notification.php">Notification </a></li>
             <li class="luxbar-item"><a href="logout.php">Logout</a></li>
