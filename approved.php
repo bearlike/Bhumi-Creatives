1b12d5846ecbe2ebe21374d32236413a6d17285a
gsnvsuraj <34811429+gsnvsuraj@users.noreply.github.com>
2019-06-10 12:03:05 +0530

    Separate header files modification

--- approved.php
+++ approved.php
@@ -9,41 +9,32 @@
 	<link rel="stylesheet" type="text/css" href="styles/styles.css">
 </head>
 <body>
-	<ul>
-  		<li><a href="projectsAdmin.php">Home</a></li>
-  		<li><a class="active" href="approval.php">Requets</a></li>
-  		<li style="float:right"><a href="logout.php">LogOut</a></li>
-        <li style="float:right"><a href="notificationAdmin.php">All Notifications</a></li>
-	</ul>
-
 
 	<?php
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
-		$dbname = "uplabs";
-		// Create connection
-		$con = new mysqli($servername, $username, $password, $dbname);
+        include 'headerAdmin.php';
+		include 'connection.php';
 
-		// Check connection
-		if ($con->connect_error) {
-			die("Connection failed: " . $con->connect_error);
-		}
+        session_start();
+
+        if(isset($_SESSION['user']) && $_SESSION['user'] == "admin")
+            $user = $_SESSION['user'];
+        else
+            header("Location:loginAdmin.php");
     
         $stat = $_GET['stat'];
         $pid  = $_GET['pid'];
         
         $sql = "SELECT * FROM approval WHERE pid ='".$pid."';";
-        $result = $con->query($sql);
+        $result = $conn->query($sql);
         $row2 = $result->fetch_assoc();
         
         if($stat == 'D')
         {
-            $query2="insert into notification values('".$row2['user']."','".$row2['title']." has been declined',CURDATE(),'unread');";
-            $con->query($query2);
+            $query2="INSERT INTO notification VALUES('".$row2['user']."','".$row2['title']." has been declined',CURDATE(),'unread');";
+            $conn->query($query2);
             
-            $query3="delete from approval where pid='".$pid."';";
-            $con->query($query3);
+            $query3="DELETE FROM approval WHERE pid='".$pid."';";
+            $conn->query($query3);
 
             if (!unlink($row2['image']))
                 echo ("Error deleting image");
@@ -55,21 +46,22 @@
         
         if($stat == 'A')
         {
-            $query2="insert into notification values('".$row2['user']."','".$row2['title']." has been approved',CURDATE(),'unread');";
-            $con->query($query2);
+            $query2="INSERT INTO notification VALUES('".$row2['user']."','".$row2['title']." has been approved',CURDATE(),'unread');";
+            $conn->query($query2);
             
-            $query4="insert into project values('".$pid."','".$row2['user']."','".$row2['title']."','".$row2['image']."','".$row2['city']."','".$row2['tags']."','".$row2['source_file']."',0) ;";
-            $con->query($query4);
+            $query4="INSERT INTO project VALUES('".$pid."','".$row2['user']."','".$row2['title']."','".$row2['image']."','".$row2['city']."','".$row2['tags']."','".$row2['source_file']."',0) ;";
+            $conn->query($query4);
 
-            $query5="delete from approval where pid='".$pid."';";
-            $con->query($query5);
+            $query5="DELETE FROM approval WHERE pid='".$pid."';";
+            $conn->query($query5);
         }
+        
 		$sql = "SELECT * FROM approval;";
-		$result = $con->query($sql);
+		$result = $conn->query($sql);
 
 		if ($result->num_rows > 0){
-      echo "<table><tr>";
-      $id=0;
+            echo "<table><tr>";
+            $id=0;
 			while($row = $result->fetch_assoc()){
 				$title = $row["title"];
 				$url = $row["image"];
@@ -86,6 +78,7 @@
 		else{
 			echo "<h3>No Projects to Display.</h3>";
 		}
+        $conn->close();
 	?>
 
 	
