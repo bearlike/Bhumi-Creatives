910a294574b84828c0cedf08e70c1fa202c40841
gsnvsuraj <34811429+gsnvsuraj@users.noreply.github.com>
2019-06-10 12:01:39 +0530

    Added header files

---
+++ headerAdmin.php
@@ -0,0 +1,10 @@
+<link rel="icon" type="image/ico" href="images/logo.png" />
+<ul>
+  		<li><a href="projectsAdmin.php">Home</a></li>
+  		<li><a href="approval.php">Requets</a></li>
+  		<li><a><form action='filteredAdmin.php' method='post'><input type='text' name='filter' placeholder='Filter by tags' required/>
+            <input type='submit' value='Filter' />
+ 		</form></a></li>
+  		<li style="float:right"><a href="logout.php">LogOut</a></li>
+  		<li style="float:right"><a href="notificationAdmin.php">All Notifications</a></li>
+	</ul>
