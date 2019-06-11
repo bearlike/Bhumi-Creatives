3253506b14e6851808aef7d1f5f3b8a4005c430c
Krishnakanth Alagiri <39209037+bearlike@users.noreply.github.com>
2019-06-11 15:16:48 +0530

    Fix Styles

--- resetPassPro.php
+++ resetPassPro.php
@@ -3,12 +3,19 @@
 <head>
 	<title>Reset Password</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<link rel="stylesheet" type="text/css" href="styles/styles.css">
 	<link rel="icon" type="image/png" href="assets/img/siteicon.png" />
+	<!-- CSS Stylesheets Here -->
+	<link rel="stylesheet" type="text/css" href="assets/css/login.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/footer.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/my-login.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
+	<!-- Fonts Here -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 </head>
 <body>
-
+	<div class="my-login-page">
+	<!-- PHP Open -->
 	<?php
 		session_start();
 
@@ -41,16 +48,23 @@
 			}
 			$conn->close();
 		}
-
 	?>
-
-	<center>
-		<form method="post" action="resetPassPro.php" class="log">
-			<h3>RESET PASSWORD</h3>
-			<br>New Password : <input type="password" name="pass" placeholder="Enter Password" required/><br><br>
-			Confirm New Password : <input type="password" name="conPass" placeholder="Enter Confirm Password" required/><br><br>
-			<input type="submit" name="subPass" value="Change Password" class="button" />
-		</form>
+	<!-- PHP Close -->
+	<section class="h-100">
+		<div class="container h-100">
+			<div class="row justify-content-md-center h-100">
+				<div class="card-wrapper">
+					<div class="brand">
+						<img src="assets/img/logo.jpg" alt="Bhumi logo">
+					</div>
+					<div class="card fat">
+						<div class="card-body">
+							<form action="loginAdmin.php" method="POST">
+								<h4 class="card-title">Moderator Login</h2>
+									<input type="password" name="pass" placeholder="Enter Password" required/><br>
+									<input type="password" name="conPass" placeholder="Enter Confirm Password" required/><br><br>
+									<input type="submit" name="subPass" value="Change Password"class="btn btn-primary btn-block" />
+								</form>
 	</center>
 
 </body>
