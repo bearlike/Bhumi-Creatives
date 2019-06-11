3cb885d570a442cda602a0c720d961608b82dd0f
Krishna Alagiri (KK) <krishna.alagiri03@gmail.com>
2019-06-12 00:07:17 +0530

    Fix Errors

--- notification.php
+++ notification.php
@@ -4,35 +4,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Projects</title>
-
+	<title>Notification</title>
  	<link rel="stylesheet" type="text/css" href="styles/styles.css">
-
 </head>
+
 <body>
 
 	<?php
 		include 'header.php';
 		include 'connection.php';
-
 		session_start();
-
 		if(isset($_SESSION['user']))
 			$user = $_SESSION['user'];
 		else
 			header("Location:index.php");
-
 		$sql = "SELECT * FROM notification WHERE uname='".$user."';";
 		$result = $conn->query($sql) or die($conn->error);
-
 		if ($result->num_rows > 0){
 			echo "<h3 class='w3-container'>Notifications are -</h3>";
 			while($row = $result->fetch_assoc()){
 				$message = $row['message'];
 				$dates = $row['dateCheck'];
-
-				echo "<br><div class='w3-container'>".$dates."<br><b>Message</b> - ".$message."</div><br><br>";
-
+				echo "<br><div class='w3-container'>".$dates."<br><b>Message</b>: ".$message."</div>";
 				$sql = "UPDATE notification SET status='read' WHERE uname='".$user."';";
 				$result1 = $conn->query($sql);
 			}
